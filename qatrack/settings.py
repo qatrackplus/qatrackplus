700c35df60edf45098cf31e65b2d49a213784d21
Randle Taylor <randle.taylor@gmail.com>
2012-03-17 00:44:13 -0400

    added login required middleware

--- qatrack/settings.py
+++ qatrack/settings.py
@@ -107,6 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'middleware.login_required.LoginRequiredMiddleware',
 )
 
 ROOT_URLCONF = 'qatrack.urls'
