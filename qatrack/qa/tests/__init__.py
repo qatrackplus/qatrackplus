ae65ee740f7b235d166c1aa59816af411e3842bf
Randle Taylor <randle.taylor@gmail.com>
2012-07-09 23:46:44 -0400

    beginning of testing views

--- qatrack/qa/tests/__init__.py
+++ qatrack/qa/tests/__init__.py
@@ -1,7 +1,7 @@
-#from qatrack.qa.tests.test_views import *
+from qatrack.qa.tests.test_views import *
 from qatrack.qa.tests.test_models import *
 
 __test__ = {
-#    "views": ["test_views"],
+    "views": ["test_views"],
     "models":["test_models"],
 }
