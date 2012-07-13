d26b739bec5c05556a5ac05f80e62280fbe52368
Randle Taylor <randle.taylor@gmail.com>
2012-07-13 13:48:22 -0400

    added filter prototype to base.js

--- qatrack/qa/static/js/base.js
+++ qatrack/qa/static/js/base.js
@@ -91,3 +91,29 @@ $(document).ready(function(){
 		});
 	});
 });
+
+
+/* add filter to IE*/
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array();
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+      {
+        var val = this[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, this))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
+}
