f98dc5827d62f673debe8e6a9f6cf8034fb32637
Randle Taylor <randle.taylor@gmail.com>
2012-07-30 13:26:14 -0400

    update so ordering of testlist cycles works in admin

--- qatrack/qa/admin.py
+++ qatrack/qa/admin.py
@@ -254,11 +254,11 @@ class UnitTestCollectionAdmin(admin.ModelAdmin):
     change_form_template = "admin/treenav/menuitem/change_form.html"
 
 #============================================================================
-class TestListCycleMembershipInline(admin.TabularInline):
+class TestListCycleMembershipInline(SalmonellaMixin,admin.TabularInline):
 
     model = models.TestListCycleMembership
     extra = 0
-
+    salmonella_fields = ("test_list",)
 
 #============================================================================
 class TestListCycleAdmin(SaveUserMixin, admin.ModelAdmin):
@@ -266,6 +266,7 @@ class TestListCycleAdmin(SaveUserMixin, admin.ModelAdmin):
     inlines = [TestListCycleMembershipInline]
     prepopulated_fields =  {'slug': ('name',)}
     search_fields = ("name","slug",)
+
     #============================================================================
     class Media:
         js = (
