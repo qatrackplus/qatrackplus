51d40d002aad50ae34bda1d6a8a0e74683b798a0
Randle Taylor <randle.taylor@gmail.com>
2012-08-01 10:05:39 -0400

    bump version

--- qatrack/settings.py
+++ qatrack/settings.py
@@ -21,7 +21,7 @@ SEND_BROKEN_LINK_EMAILS = True
 #misc settings
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'qatrack.wsgi.application'
