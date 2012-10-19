b461227fd38d8c96e95bb0265049bdca9e29fda7
Randle Taylor <randle.taylor@gmail.com>
2012-10-19 15:48:31 -0400

    display all units of same type under single header

--- qatrack/qa/views.py
+++ qatrack/qa/views.py
@@ -468,12 +468,21 @@ class CompositeCalculation(JSONResponseMixin, View):
 #====================================================================================
 class ChooseUnit(ListView):
     """choose a unit to perform qa on for this session"""
-    model = Unit
-    context_object_name = "units"
+    model = UnitType
+    context_object_name = "unit_types"
 
     #---------------------------------------------------------------------------
     def get_queryset(self):
-        return Unit.objects.all().select_related("type")
+        return UnitType.objects.all().order_by("unit__number").prefetch_related("unit_set")
+    #----------------------------------------------------------------------
+    def get_context_data(self,*args,**kwargs):
+        """reorder unit types"""
+        context = super(ChooseUnit,self).get_context_data(*args,**kwargs)
+        uts = [ut for ut in context["unit_types"] if len(ut.unit_set.all()) >0]
+        context["unit_types"] = utils.unique(uts)
+        return context
+
+
 
 #============================================================================
 class PerformQA(CreateView):
