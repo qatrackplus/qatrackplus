bdf6065e23a59a00719059c590e297c5af2d707f
Randle Taylor <randle.taylor@gmail.com>
2012-11-23 16:46:31 -0500

    check for invalid values in guass_pdf

--- qatrack/qa/control_chart/leastsquaresfit.py
+++ qatrack/qa/control_chart/leastsquaresfit.py
@@ -7,8 +7,8 @@ from scipy.optimize import curve_fit
 
 
 def gauss_fit(data, freq, bins, binwidth):
-    """ 
-    Fits a Gaussian pdf to a histogram using 
+    """
+    Fits a Gaussian pdf to a histogram using
     the method of least squares.
     data = array of binned values
     freq = array of values for each bin
@@ -17,7 +17,7 @@ def gauss_fit(data, freq, bins, binwidth):
     """
     x = bincenters(bins)
     norm = np.float(np.size(data) * binwidth)
-    
+
     initGuess = [norm, np.mean(data), np.std(data)]
     optParam, cov = curve_fit(gauss_pdf, x, freq, initGuess)
 
@@ -33,15 +33,18 @@ def bincenters(bins):
     bincenters = 0.5 * (bins[1: ] + bins[:- 1])
     return bincenters
 
-    
-    
+
+
 def gauss_pdf(x, norm, mu, sigma):
-    """ 
+    """
     The method calculates the value of the Gaussian probability
     density function (using matplotlib routines) for a value/array x.
     for a given mean (mu) and standard deviation (sigma). The results
     is normalized (multiplied by) 'norm', and so 'norm' should equal
     1.000 unless you have a reason for it to be otherwise.
     """
+    if any(np.isnan([x,mu,sigma])):
+        raise Exception("Invalid value in gauss_pdf")
+
     GaussPdf = norm * mlab.normpdf(x, mu, sigma)
     return GaussPdf
