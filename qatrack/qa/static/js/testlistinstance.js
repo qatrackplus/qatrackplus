63afcb00385251c6b167b03fb59e671996e0622c
Randle Taylor <randle.taylor@gmail.com>
2012-11-29 10:51:53 -0500

    cleanup and add server delay

--- qatrack/qa/static/js/testlistinstance.js
+++ qatrack/qa/static/js/testlistinstance.js
@@ -1,15 +1,3 @@
-var supress_initial_reload = true;
-var column_sort_keys = [
-    null,
-    "unit_test_collection__unit__name",
-    "unit_test_collection__frequency__nominal_interval",
-    "test_list__name",
-    "work_completed",
-    "created_by__username",
-    null,
-    null
-];
-
 /**************************************************************************/
 //Initialize sortable/filterable test list table data types
 function init_test_list_instance_tables(units, frequencies){
@@ -53,75 +41,13 @@ function init_test_list_instance_tables(units, frequencies){
 
     }).columnFilter({
         sPlaceHolder: "head:after",
-        aoColumns: filter_cols
-    }).fnSetFilteringDelay();
+        aoColumns: filter_cols,
+        iFilteringDelay:250
+    });
 
     $("#testlistinstance-table").find("select, input").addClass("input-small");
-/*
-    var searchWait = 0;
-    var searchWaitInterval;
-    $('.dataTables_filter input')
-    .unbind('keypress keyup')
-    .bind('keypress keyup', function(e){
-        var item = $(this);
-        searchWait = 0;
-        if(!searchWaitInterval) searchWaitInterval = setInterval(function(){
-            if(searchWait>=3){
-                clearInterval(searchWaitInterval);
-                searchWaitInterval = '';
-                searchTerm = $(item).val();
-                oTable.fnFilter(searchTerm);
-                searchWait = 0;
-            }
-            searchWait++;
-        },200);
-
-    });
-    */
 }
 
-function on_table_sort(){
-
-    if (supress_initial_reload){
-        supress_initial_reload = false;
-        return;
-    }
-
-    var sorts = this.fnSettings().aaSorting;
-    var qs = "?";
-    var refresh_required = false;
-
-    _.each(sorts,function(sort_info){
-
-    var col = sort_info[0];
-    var dir = sort_info[1];
-    var key = column_sort_keys[col];
-
-    var ordering;
-
-    if (!_.isNull(key)){
-
-    refresh_required = true;
-
-    if (dir === "desc"){
-        key = "-"+key;
-    }
-
-    qs += "order="+key+"&";
-}
-});
-
-    if (refresh_required){
-        var url = document.location.href.replace(document.location.search,"")+qs;
-        document.location.replace(url);
-    }
-
-
-}
-
-function get_units(){
-
-}
 /**************************************************************************/
 $(document).ready(function(){
 
