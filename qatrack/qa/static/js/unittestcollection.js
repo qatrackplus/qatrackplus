dc19703bef90993eae708f3c79df111e28b4cbe9
Randle Taylor <randle.taylor@gmail.com>
2012-11-30 00:11:15 -0500

    clean up for datatables

--- qatrack/qa/static/js/unittestcollection.js
+++ qatrack/qa/static/js/unittestcollection.js
@@ -8,12 +8,12 @@ function init_test_collection_tables(units, frequencies){
 
     var cols = [
         {bSortable:false},
-        null,  // Test list name
-        {"sType":"span-timestamp"}, //due date
+        {bSortable:false},
+        null, //due date
         null, //Unit
         null, //Freq
         null,//assigned to
-        {"sType":"span-timestamp"}, //date completed
+        null, //date completed
         {bSortable:false},
         {bSortable:false}
     ];
@@ -64,65 +64,3 @@ $(document).ready(function(){
 
 });
 
-/*
-
-
-//Initialize sortable/filterable test list table data types
-function init_test_collection_tables(){
-    $(".test-collection-table").each(function(idx,table){
-
-    if ($(this).find("tr.empty-table").length>0){
-        return;
-    }
-
-    var cols = [
-        null, //Action
-        null,  // Test list name
-        {"sType":"span-timestamp"}, //due date
-        null, //Unit
-        null, //Freq
-        null,//assigned to
-        {"sType":"span-timestamp"}, //date completed
-        null, //pass/fail status
-        null // review -status
-    ];
-
-    var filter_cols = [
-        null, //action
-        {type: "text" }, // Test list name
-        {type: "text" }, //due date
-        {type: "select"}, //Unit
-        {type: "select"}, //Freq
-        {type: "select"},//assigned to
-        {type: "text" }, //date completed
-        null, //pass/fail status
-        null //review-status
-    ];
-
-
-    $(table).dataTable( {
-        sDom: "t",
-        bStateSave:false, //save filter/sort state between page loads
-        bFilter:true,
-        bPaginate: false,
-        aaSorting:[[3,"asc"],[1,"asc"]],//sort by unit then name
-        aoColumns: cols,
-        fnAdjustColumnSizing:false
-
-    }).columnFilter({
-        sPlaceHolder: "head:after",
-        aoColumns: filter_cols
-    });
-
-    $(table).find("select, input").addClass("input-small");
-
-    });
-
-}
-
-
-$(document).ready(function(){
-    init_test_collection_tables();
-});
-
-*/
