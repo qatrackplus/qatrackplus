194d10891d581b2cae06858e10e6a4809bb4ddb9
Randle Taylor <randle.taylor@gmail.com>
2013-01-10 15:18:58 -0500

    new filtering on overview page

--- qatrack/qa/static/js/overview.js
+++ qatrack/qa/static/js/overview.js
@@ -1,40 +1,23 @@
-function toggle_image_class(img){
-       var to_add, to_rem;
+function unit_container_id(option){
+    return "#unit-container-"+$(option).val();
+}
+function filter_units(){
+    var units_to_show;
+    var units_to_hide;
 
-        if (img.hasClass("icon-minus-sign")){
-            to_add = "icon-plus-sign";
-        }else{
-            to_add = "icon-minus-sign";
-        }
+    if ($("#all-option").is(":selected")){
+        units_to_show = _.map($(".unit-option"),unit_container_id);
+        units_to_hide = [];
+    }else{
+        units_to_show = _.map($("#unit-filter option:selected"),unit_container_id)
+        units_to_hide = _.map($("#unit-filter option").not(":selected"),unit_container_id);
+    }
 
-        img.removeClass("icon-minus-sign icon-plus-sign").addClass(to_add);
+    $(units_to_show.join(",")).show();
+    $(units_to_hide.join(",")).hide();
 }
 /**************************************************************************/
 $(document).ready(function(){
-    $("span.toggle-unit").click(function(){
-        $(this).parent().parent().next("div.unit-container").toggle();
-        toggle_image_class($(this).find("i"))
-    });
-
-    $("#collapse-container").click(function(){
-        var img = $(this).find("i");
-        var txt = $("#collapse-string");
-        var to_toggle;
-
-        var containers = $("div.unit-container");
-
-        if (img.hasClass("icon-minus-sign")){
-            containers.hide();
-            txt.text("Show All");
-            to_toggle = ".icon-minus-sign";
-        }else{
-            containers.show();
-            txt.text("Collapse All");
-            to_toggle = ".icon-plus-sign";
-        }
-
-        toggle_image_class($(to_toggle));
-
-    });
+    $("#unit-filter").change(filter_units);
 });
 
