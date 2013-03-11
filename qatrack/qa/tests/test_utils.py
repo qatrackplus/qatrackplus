3dda303f2f7282492ea02c60ec7bca2dd58b8f86
Randle Taylor <randle.taylor@gmail.com>
2013-03-11 14:21:04 -0400

    test utils added

---
+++ qatrack/qa/tests/test_utils.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from qatrack.qa import utils
+
+#============================================================================
+class TestUtils(TestCase):
+
+    #----------------------------------------------------------------------
+    def test_unique(self):
+        items = ["foo","foo","bar"]
+        self.assertListEqual(items[1:],utils.unique(items))
