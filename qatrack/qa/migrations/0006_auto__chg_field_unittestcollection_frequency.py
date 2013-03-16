165ca19c8c6d0c332f3c6eb8fe8b836394ac2c65
Randle Taylor <randle.taylor@gmail.com>
2013-03-15 20:25:11 -0400

    add special case for SQL Server

--- qatrack/qa/migrations/0006_auto__chg_field_unittestcollection_frequency.py
+++ qatrack/qa/migrations/0006_auto__chg_field_unittestcollection_frequency.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import datetime
-from south.db import db
+from south.db import db,engine
 from south.v2 import SchemaMigration
 from django.db import models
 
@@ -10,12 +10,19 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
 
         # Changing field 'UnitTestCollection.frequency'
-        db.alter_column('qa_unittestcollection', 'frequency_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['qa.Frequency'], null=True))
-
+        if "sql_server" not in engine:
+            db.alter_column('qa_unittestcollection', 'frequency_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['qa.Frequency'], null=True))
+        else:
+            db.execute("ALTER TABLE qa_unittestcollection ALTER COLUMN frequency_id int NULL")            
+        
+        db
     def backwards(self, orm):
-
+        
         # Changing field 'UnitTestCollection.frequency'
-        db.alter_column('qa_unittestcollection', 'frequency_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['qa.Frequency']))
+        if "sql_server" not in engine:
+            db.alter_column('qa_unittestcollection', 'frequency_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['qa.Frequency']))
+        else:
+            db.execute("ALTER TABLE qa_unittestcollection ALTER COLUMN frequency_id int NOT NULL")                        
 
     models = {
         'auth.group': {
