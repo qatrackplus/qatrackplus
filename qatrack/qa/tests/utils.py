497e4040ac8ab55f43c965164d8a35b52a3dc6b3
randlet <randle.taylor@gmail.com>
2013-06-03 12:30:53 -0400

    formatting for pep8

--- qatrack/qa/tests/utils.py
+++ qatrack/qa/tests/utils.py
@@ -9,12 +9,10 @@ from django.utils import unittest, timezone
 
 from django.contrib.auth.models import User, Group
 from qatrack.qa import models
-from qatrack.qa import *
-from qatrack.units.models import Unit, UnitType, Modality, ELECTRON, PHOTON
+from qatrack.units.models import Unit, UnitType, Modality, PHOTON
+
 
 #----------------------------------------------------------------------
-
-
 def create_user(is_staff=True, is_superuser=True, uname="user", pwd="password"):
     try:
         u = User.objects.get(username=uname)
@@ -25,24 +23,23 @@ def create_user(is_staff=True, is_superuser=True, uname="user", pwd="password"):
         u.set_password(pwd)
         u.save()
     return u
+
+
 #----------------------------------------------------------------------
-
-
 def create_category(name="cat"):
     c, _ = models.Category.objects.get_or_create(name=name, slug="cat", description="cat")
     c.save()
     return c
+
+
 #----------------------------------------------------------------------
-
-
 def create_status(name="status", slug="status", is_default=True):
     status = models.TestInstanceStatus(name=name, slug=slug, is_default=is_default, requires_review=True)
     status.save()
     return status
 
+
 #----------------------------------------------------------------------
-
-
 def create_test(name=None, test_type=models.SIMPLE):
     user = create_user()
     if name is None or models.Test.objects.filter(name=name).count() > 0:
@@ -58,9 +55,9 @@ def create_test(name=None, test_type=models.SIMPLE):
     )
     test.save()
     return test
+
+
 #----------------------------------------------------------------------
-
-
 def create_test_list(name="test_list"):
     user = create_user()
     test_list = models.TestList(
@@ -72,9 +69,9 @@ def create_test_list(name="test_list"):
     )
     test_list.save()
     return test_list
+
+
 #----------------------------------------------------------------------
-
-
 def create_test_list_instance(unit_test_collection=None, work_completed=None, created_by=None, test_list=None):
     if unit_test_collection is None:
         unit_test_collection = create_unit_test_collection()
@@ -97,9 +94,8 @@ def create_test_list_instance(unit_test_collection=None, work_completed=None, cr
     tli.save()
     return tli
 
+
 #----------------------------------------------------------------------
-
-
 def create_cycle(test_lists=None, name="cycle"):
     user = create_user()
     cycle = models.TestListCycle(
@@ -120,17 +116,15 @@ def create_cycle(test_lists=None, name="cycle"):
 
     return cycle
 
+
 #----------------------------------------------------------------------
-
-
 def create_test_list_membership(test_list, test, order=0):
     tlm = models.TestListMembership(test_list=test_list, test=test, order=order)
     tlm.save()
     return tlm
 
+
 #----------------------------------------------------------------------
-
-
 def create_test_instance(unit_test_info=None, value=1., created_by=None, work_completed=None, status=None):
     if unit_test_info is None:
         unit_test_info = create_unit_test_info()
@@ -156,24 +150,23 @@ def create_test_instance(unit_test_info=None, value=1., created_by=None, work_co
 
     ti.save()
     return ti
+
+
 #----------------------------------------------------------------------
-
-
 def create_modality(energy=6, particle=PHOTON):
     m, _ = Modality.objects.get_or_create(type=particle, energy=energy)
     m.save()
     return m
+
+
 #----------------------------------------------------------------------
-
-
 def create_unit_type(name="utype", vendor="vendor", model="model"):
     ut, _ = UnitType.objects.get_or_create(name=name, vendor=vendor, model=model)
     ut.save()
     return ut
 
+
 #----------------------------------------------------------------------
-
-
 def create_unit(name="unit", number=1):
     u = Unit(name=name, number=number)
     u.type = create_unit_type()
@@ -181,9 +174,9 @@ def create_unit(name="unit", number=1):
     u.modalities.add(create_modality())
     u.save()
     return u
+
+
 #----------------------------------------------------------------------
-
-
 def create_reference(name="ref", ref_type=models.NUMERICAL, value=1, created_by=None):
     if created_by is None:
         created_by = create_user()
@@ -194,9 +187,9 @@ def create_reference(name="ref", ref_type=models.NUMERICAL, value=1, created_by=
     )
     r.save()
     return r
+
+
 #----------------------------------------------------------------------
-
-
 def create_tolerance(tol_type=models.ABSOLUTE, act_low=-2, tol_low=-1, tol_high=1, act_high=2, created_by=None):
     if created_by is None:
         created_by = create_user()
@@ -211,16 +204,15 @@ def create_tolerance(tol_type=models.ABSOLUTE, act_low=-2, tol_low=-1, tol_high=
     tol.save()
     return tol
 
+
 #----------------------------------------------------------------------
-
-
 def create_group(name="group"):
     g = Group(name=name)
     g.save()
     return g
+
+
 #----------------------------------------------------------------------
-
-
 def create_frequency(name="freq", slug="freq", nom=1, due=1, overdue=1):
     f = models.Frequency(
         name=name, slug=slug,
@@ -228,9 +220,9 @@ def create_frequency(name="freq", slug="freq", nom=1, due=1, overdue=1):
     )
     f.save()
     return f
+
+
 #----------------------------------------------------------------------
-
-
 def create_unit_test_info(unit=None, test=None, assigned_to=None, ref=None, tol=None, active=True):
 
     if unit is None:
@@ -253,9 +245,8 @@ def create_unit_test_info(unit=None, test=None, assigned_to=None, ref=None, tol=
     uti.save()
     return uti
 
+
 #----------------------------------------------------------------------
-
-
 def create_unit_test_collection(unit=None, frequency=None, test_collection=None, assigned_to=None, null_frequency=False):
 
     if unit is None:
@@ -287,4 +278,4 @@ def create_unit_test_collection(unit=None, frequency=None, test_collection=None,
 #----------------------------------------------------------------------
 def datetimes_same(date1, date2, nminutes=1):
     """return whether date1 and date2 are the same within nminutes minutes"""
-    return abs(date1-date2) <= timezone.timedelta(minutes=nminutes)
+    return abs(date1 - date2) <= timezone.timedelta(minutes=nminutes)
