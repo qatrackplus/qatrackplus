912ec12243c4b18e578934320b627802b2f55fb2
randlet <randle.taylor@gmail.com>
2013-07-04 10:29:39 -0400

    fix test selection due to jquery.live being removed

--- qatrack/qa/static/js/review_test_list.js
+++ qatrack/qa/static/js/review_test_list.js
@@ -2,14 +2,14 @@
 $(document).ready(function(){
 
 	//(de)select checkboxes for child tests when user clicks on header checkbox
-	$("input.test-selected-toggle").live("change",function(e){
-		$(this).closest("table").find("input.test-selected").attr("checked",$(this).is(":checked"))
+	$("input.test-selected-toggle").on("change",function(e){
+		$(this).closest("table").find("input.test-selected").prop("checked",$(this).is(":checked"))
 	});
 
 	$("#test-list-info-toggle").click(function(){
 		$("#test-list-info").toggle(600);
 	});
-	$("#bulk-status").live('change',function(){
+	$("#bulk-status").on('change',function(){
 		var val = $("#bulk-status").val();
 		if (val !== ""){
 			$("input.test-selected:checked").parents("tr").find("select").val(val);
