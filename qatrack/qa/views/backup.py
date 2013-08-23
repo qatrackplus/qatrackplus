91a9800c04dbe8bd176b974c25a697ba32e5a311
randlet <randle.taylor@gmail.com>
2013-08-22 20:35:16 -0400

    improve docs for backup

--- qatrack/qa/views/backup.py
+++ qatrack/qa/views/backup.py
@@ -12,28 +12,35 @@ from qatrack.units.models import Unit
 
 
 class PaperBackupRequestForm(forms.Form):
+    """Form for choosing paper backup options"""
+
     units = forms.models.ModelMultipleChoiceField(
         queryset=Unit.objects,
         help_text=_("Choose which units to include")
     )
+
     frequencies = forms.models.ModelMultipleChoiceField(
         queryset=models.Frequency.objects,
         help_text=_("Choose which test list frequencies to include"),
     )
+
     test_categories = forms.models.ModelMultipleChoiceField(
         queryset=models.Category.objects,
         help_text=_("Choose which test categories to include"),
     )
+
     assigned_to = forms.models.ModelMultipleChoiceField(
         queryset=Group.objects,
         help_text=_("Filter test lists by who they are assigned to"),
     )
+
     include_refs = forms.BooleanField(
         label=_("References & Tolerances"),
         initial=True,
         required=False,
         help_text=_("Display references and tolerances in forms"),
     )
+
     include_inactive = forms.BooleanField(
         help_text=_("Include Inactive Test Lists"),
         initial=False,
@@ -43,12 +50,14 @@ class PaperBackupRequestForm(forms.Form):
 
 #============================================================================
 class PaperFormRequest(FormView):
+    """View for displaying paper backup options form"""
 
     form_class = PaperBackupRequestForm
     template_name = "qa/backup_form.html"
 
     #---------------------------------------------------------------
     def get_initial(self):
+        """setup some hopefully sensible default options"""
 
         if self.request.method == "GET":
             return {
@@ -63,6 +72,10 @@ class PaperFormRequest(FormView):
 
     #---------------------------------------------------------------
     def form_valid(self, form):
+        """
+        url encode all requested options and redirect to the actual
+        PaperForms view.
+        """
 
         q = urlencode({
             "unit": form.cleaned_data["units"].values_list("pk", flat=True),
@@ -78,12 +91,15 @@ class PaperFormRequest(FormView):
 
 #============================================================================
 class PaperForms(ListView):
+    """ View that handles the actual creation of the forms"""
+
     model = models.UnitTestCollection
 
     template_name_suffix = "_backup"
 
     #---------------------------------------------------------------
     def get_queryset(self):
+        """filter queryset based on requested options"""
 
         qs = super(PaperForms, self).get_queryset().filter(
             unit__pk__in=self.request.GET.getlist("unit"),
@@ -98,18 +114,27 @@ class PaperForms(ListView):
 
     #---------------------------------------------------------------
     def get_context_data(self, *args, **kwargs):
+        """
+        Patch each :model:`qa.UnitTestCollection` object with all
+        its relevant TestList's, Test's & UnitTestInfo's.
+        """
 
         context = super(PaperForms, self).get_context_data(*args, **kwargs)
 
         context["include_refs"] = self.request.GET.get("include_refs", "True") != "False"
+
         test_lists = {}
 
         for utc in context["object_list"]:
             key = (utc.content_type_id, utc.object_id)
 
             try:
+                # if we've already seen this test list or test list cycle we don't need to
+                # fetch all the test list members & tests again
                 all_lists = test_lists[key]
             except KeyError:
+                # first time we've seen this test list or test list cycle
+                # find  all member test lists and tests
                 all_lists = utc.tests_object.test_list_members().prefetch_related("testlistmembership_set__test")
                 for li in all_lists:
                     li.all_tests = li.ordered_tests()
@@ -124,6 +149,9 @@ class PaperForms(ListView):
                 ).select_related(
                     "test", "reference", "tolerance"
                 )
+
                 li.utis = list(sorted(utis, key=lambda uti: li.all_tests.index(uti.test)))
+
             utc.all_lists = all_lists
+
         return context
