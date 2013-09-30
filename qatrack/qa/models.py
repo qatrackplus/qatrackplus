471e66e784474f462601b1ea1a827ad66d21ab6a
randlet <randle.taylor@gmail.com>
2013-09-30 09:43:57 -0400

    add ordering for category

--- qatrack/qa/models.py
+++ qatrack/qa/models.py
@@ -416,6 +416,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "categories"
+        ordering = ("name",)
 
     #----------------------------------------------------------------------
     def __unicode__(self):
