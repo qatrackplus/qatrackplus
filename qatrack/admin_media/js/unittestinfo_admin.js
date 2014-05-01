2c58e78213c3b09312d3a638ed70d94285f5ee49
randlet <randle.taylor@gmail.com>
2014-05-01 14:50:36 -0400

    switch to named urls, switch to site wide jquery and a couple of other minor changes to the js

--- qatrack/admin_media/js/unittestinfo_admin.js
+++ qatrack/admin_media/js/unittestinfo_admin.js
@@ -1,42 +1,64 @@
+"use strict";
+
 $(document).ready(function() {
+
     $("select[name='dest_unit'] > option").remove();
     $("select[name='testlist'] > option").remove();
-    var tempGetTestListUrl = ADMINURLs.GETTESTLIST;
-    var tempGetDestUnitUrl = ADMINURLs.GETDESTUNIT;
 
-    GetTestLists = function() {
+    var content_type, source_unit, testlist;
+
+    var getTestLists = function() {
+
         $("select[name='dest_unit'] > option").remove();
         $("select[name='testlist'] > option").remove();
-        var testListUrl= tempGetTestListUrl + source_unit + '/' + content_type;
-        $.getJSON(testListUrl,
-            function(data) {
-                var dropdown = $("select[name='testlist']");
-                $("select[name='testlist'] > option").remove();
-                dropdown.append("<option selected='selected'>---------</option>");
-                $.each(data, function() {
-                    dropdown.append($("<option />").text(this));
-                });
 
-        });
+        if (content_type && source_unit) {
+
+            var testListUrl= ADMINURLs.GETTESTLIST.replace(
+                ":source_unit:", source_unit
+            ).replace(
+                ":content_type:", content_type
+            );
+
+            $.getJSON(testListUrl,
+                function(data) {
+                    var dropdown = $("select[name='testlist']");
+                    $("select[name='testlist'] > option").remove();
+                    dropdown.append("<option selected='selected'>---------</option>");
+                    $.each(data, function() {
+                        dropdown.append($("<option />").text(this));
+                    });
+            });
+        }
     };
 
-    GetDestUnit = function() {
-        var getDestUnitUrl = tempGetDestUnitUrl + source_unit + '/' + content_type + '/' + testlist;
-        $.getJSON(getDestUnitUrl,
-            function(data) {
-                var dropdown = $("select[name='dest_unit']");
-                $("select[name='dest_unit'] > option").remove();
-                $.each(data, function() {
-                    dropdown.append($("<option />").text(this));
-                });
+    var getDestUnit = function() {
 
-        });
+        if (content_type && source_unit) {
+
+            var getDestUnitUrl = ADMINURLs.GETDESTUNIT.replace(
+                ":source_unit:", source_unit
+            ).replace(
+                ":content_type:", content_type
+            ).replace(
+                ":testlist:", testlist
+            );
+
+            $.getJSON(getDestUnitUrl,
+                function(data) {
+                    var dropdown = $("select[name='dest_unit']");
+                    $("select[name='dest_unit'] > option").remove();
+                    $.each(data, function() {
+                        dropdown.append($("<option />").text(this));
+                    });
+            });
+        }
     };
 
     $("select[name='source_unit']").change(function() {
         $( "select[name='source_unit'] option:selected" ).each(function() {
             source_unit = $( this ).text();
-            GetTestLists();
+            getTestLists();
         });
 
     });
@@ -44,14 +66,14 @@ $(document).ready(function() {
     $("select[name='content_type']").change(function() {
         $( "select[name='content_type'] option:selected" ).each(function() {
             content_type = $( this ).text();
-            GetTestLists();
+            getTestLists();
         });
     });
 
     $("select[name='testlist']").change(function() {
         $( "select[name='testlist'] option:selected" ).each(function() {
             testlist = $( this ).text();
-            GetDestUnit();
+            getDestUnit();
         });
     });
 
