47ad229bdb8e0def2243295185bcc752191cb339
Randle Taylor <randle.taylor@gmail.com>
2016-10-02 10:55:38 -0400

    add initial units migrations and move South migrations out of django migrations directory

--- qatrack/units/migrations/0001_initial.py
+++ qatrack/units/migrations/0001_initial.py
@@ -1,99 +1,56 @@
 # -*- coding: utf-8 -*-
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'UnitType'
-        db.create_table('units_unittype', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('vendor', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('model', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, blank=True)),
-        ))
-        db.send_create_signal('units', ['UnitType'])
+    dependencies = [
+    ]
 
-        # Adding unique constraint on 'UnitType', fields ['name', 'model']
-        db.create_unique('units_unittype', ['name', 'model'])
-
-        # Adding model 'Modality'
-        db.create_table('units_modality', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('type', self.gf('django.db.models.fields.CharField')(max_length=20)),
-            ('energy', self.gf('django.db.models.fields.FloatField')()),
-        ))
-        db.send_create_signal('units', ['Modality'])
-
-        # Adding unique constraint on 'Modality', fields ['type', 'energy']
-        db.create_unique('units_modality', ['type', 'energy'])
-
-        # Adding model 'Unit'
-        db.create_table('units_unit', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('number', self.gf('django.db.models.fields.PositiveIntegerField')(unique=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
-            ('serial_number', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
-            ('location', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
-            ('install_date', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['units.UnitType'])),
-        ))
-        db.send_create_signal('units', ['Unit'])
-
-        # Adding M2M table for field modalities on 'Unit'
-        db.create_table('units_unit_modalities', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('unit', models.ForeignKey(orm['units.unit'], null=False)),
-            ('modality', models.ForeignKey(orm['units.modality'], null=False))
-        ))
-        db.create_unique('units_unit_modalities', ['unit_id', 'modality_id'])
-
-    def backwards(self, orm):
-        # Removing unique constraint on 'Modality', fields ['type', 'energy']
-        db.delete_unique('units_modality', ['type', 'energy'])
-
-        # Removing unique constraint on 'UnitType', fields ['name', 'model']
-        db.delete_unique('units_unittype', ['name', 'model'])
-
-        # Deleting model 'UnitType'
-        db.delete_table('units_unittype')
-
-        # Deleting model 'Modality'
-        db.delete_table('units_modality')
-
-        # Deleting model 'Unit'
-        db.delete_table('units_unit')
-
-        # Removing M2M table for field modalities on 'Unit'
-        db.delete_table('units_unit_modalities')
-
-    models = {
-        'units.modality': {
-            'Meta': {'unique_together': "[('type', 'energy')]", 'object_name': 'Modality'},
-            'energy': ('django.db.models.fields.FloatField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'type': ('django.db.models.fields.CharField', [], {'max_length': '20'})
-        },
-        'units.unit': {
-            'Meta': {'ordering': "['number']", 'object_name': 'Unit'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'install_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'location': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
-            'modalities': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['units.Modality']", 'symmetrical': 'False'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
-            'number': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True'}),
-            'serial_number': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
-            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['units.UnitType']"})
-        },
-        'units.unittype': {
-            'Meta': {'unique_together': "[('name', 'model')]", 'object_name': 'UnitType'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'vendor': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['units']
+    operations = [
+        migrations.CreateModel(
+            name='Modality',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(help_text='Descriptive name for this modality', unique=True, max_length=255, verbose_name='Name')),
+            ],
+            options={
+                'verbose_name_plural': 'Modalities',
+            },
+        ),
+        migrations.CreateModel(
+            name='Unit',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('number', models.PositiveIntegerField(help_text='A unique number for this unit', unique=True)),
+                ('name', models.CharField(help_text='The display name for this unit', max_length=256)),
+                ('serial_number', models.CharField(help_text='Optional serial number', max_length=256, null=True, blank=True)),
+                ('location', models.CharField(help_text='Optional location information', max_length=256, null=True, blank=True)),
+                ('install_date', models.DateField(help_text='Optional install date', null=True, blank=True)),
+                ('modalities', models.ManyToManyField(to='units.Modality')),
+            ],
+            options={
+                'ordering': ['number'],
+            },
+        ),
+        migrations.CreateModel(
+            name='UnitType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(help_text='Name for this unit type', max_length=50)),
+                ('vendor', models.CharField(help_text='e.g. Elekta', max_length=50)),
+                ('model', models.CharField(help_text='Optional model name for this group', max_length=50, null=True, blank=True)),
+            ],
+        ),
+        migrations.AlterUniqueTogether(
+            name='unittype',
+            unique_together=set([('name', 'model')]),
+        ),
+        migrations.AddField(
+            model_name='unit',
+            name='type',
+            field=models.ForeignKey(to='units.UnitType'),
+        ),
+    ]
