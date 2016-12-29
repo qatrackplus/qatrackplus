00d1c121f595e211006437c77476775c5f10e332
Randle Taylor <randle.taylor@gmail.com>
2016-12-29 14:50:37 -0500

    fix password reset functionality

--- qatrack/accounts/urls.py
+++ qatrack/accounts/urls.py
@@ -6,5 +6,6 @@ import views
 urlpatterns = patterns('',
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': settings.LOGIN_URL}),
     url(r'^details/$', views.AccountDetails.as_view(), name="account-details"),
+    url('^', include('django.contrib.auth.urls')),
     url(r'^', include('registration.backends.default.urls')),
 )
