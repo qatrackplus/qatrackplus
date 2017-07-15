f65e2d4a815985dd261c289a0b597fafab77defc
Randle Taylor <randle.taylor@gmail.com>
2017-07-15 17:41:54 -0400

    add favicons

--- qatrack/urls.py
+++ qatrack/urls.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
+from django.contrib.staticfiles.templatetags.staticfiles import static as static_url
 
 from django.contrib import admin
 admin.autodiscover()
+
+
+favicon_view = RedirectView.as_view(url=static_url("qatrack_core/img/favicon.ico"), permanent=True)
+touch_view = RedirectView.as_view(url=static_url("qatrack_core/img/apple-touch-icon.png"), permanent=True)
 
 urlpatterns = [
 
@@ -16,7 +21,11 @@ urlpatterns = [
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^favicon\.ico$', favicon_view),
+    url(r'^apple-touch-icon\.png$', touch_view),
+
     url(r'^', include('genericdropdown.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
