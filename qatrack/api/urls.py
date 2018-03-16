8a05b5010af500cf97474f9a8e96f2f59f09f8aa
Randle Taylor <randle.taylor@gmail.com>
2018-03-15 21:24:45 -0400

    add attachments api endpoints as well as browsable root

--- qatrack/api/urls.py
+++ qatrack/api/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import include, url
-from rest_framework.authtoken import views
+from rest_framework.authtoken import views as auth_views
 from rest_framework.schemas import get_schema_view
+
+from qatrack.api import views
 
 schema_view = get_schema_view(title='QATrack+ API')
 
+
 urlpatterns = [
-    url(r'^get-token/', views.obtain_auth_token, name="get-token"),
+    url(r'^$', views.api_root, name="api-root"),
+    url(r'^get-token/', auth_views.obtain_auth_token, name="get-token"),
     url(r'^authorize/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^attachments/', include('qatrack.api.attachments.urls')),
     url(r'^auth/', include('qatrack.api.auth.urls')),
     url(r'^contenttypes/', include('qatrack.api.contenttypes.urls')),
     url(r'^units/', include('qatrack.api.units.urls')),
