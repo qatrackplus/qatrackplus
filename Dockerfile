ab79413160cb756be925277e39863f422c5653ca
Simon Biggs <me@simonbiggs.net>
2018-07-06 00:32:59 +1000

    static files working

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,1 @@
 FROM python:3.6-onbuild
-
-RUN mv /usr/src/app/deploy/docker/docker_settings.py /usr/src/app/qatrack/local_settings.py
--- deploy/docker/init_script.sh
+++ deploy/docker/init_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Running Docker Init Script"
-cp /usr/src/app/deploy/docker/docker_settings.py /usr/src/app/qatrack/local_settings.py
-echo "import runpy; runpy.run_path(\"/usr/src/app/deploy/docker/docker_utility_script.py\")" | python /usr/src/app/manage.py shell
+cp /usr/src/qatrackplus/deploy/docker/docker_settings.py /usr/src/qatrackplus/qatrack/local_settings.py
+echo "import runpy; runpy.run_path(\"/usr/src/qatrackplus/deploy/docker/docker_utility_script.py\")" | python /usr/src/qatrackplus/manage.py shell
 
 # /usr/bin/crontab deploy/docker/crontab
 gunicorn qatrack.wsgi:application -w 2 -b :8000
