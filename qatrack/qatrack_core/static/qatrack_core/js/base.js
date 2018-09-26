2b97a34ff46327d49f7fb0915c86e7ae15048005
Ryan Bottema <ryanbottema@gmail.com>
2018-09-26 12:26:57 -0400

    Perform: Prevent number changing when scrolling on focused number input.

--- qatrack/qatrack_core/static/qatrack_core/js/base.js
+++ qatrack/qatrack_core/static/qatrack_core/js/base.js
@@ -219,6 +219,15 @@ require(['jquery'], function ($) {
                 $control_sidebar.removeClass('control-sidebar-open');
             }
         }
-    )
+    );
+
+    $('form').on('focus', 'input[type=number]', function (e) {
+        $(this).on('mousewheel.disableScroll', function (e) {
+            e.preventDefault()
+        })
+    })
+    $('form').on('blur', 'input[type=number]', function (e) {
+        $(this).off('mousewheel.disableScroll')
+    })
 
 });
