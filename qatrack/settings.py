5a22eee8f22449bac58d3d7a226c2db1105bfcc8
randlet <randle.taylor@gmail.com>
2018-12-14 12:34:56 -0500

    rearrange settings a bit to allow dependent paths to be updated

--- qatrack/settings.py
+++ qatrack/settings.py
@@ -22,8 +22,6 @@ SEND_BROKEN_LINK_EMAILS = False
 # misc settings
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 LOG_ROOT = os.path.join(PROJECT_ROOT, "..", "logs")
-if not os.path.isdir(LOG_ROOT):
-    os.mkdir(LOG_ROOT)
 
 VERSION = "0.3.0.9"
 BUG_REPORT_URL = "https://bitbucket.org/tohccmedphys/qatrackplus/issues/new"
@@ -104,11 +102,6 @@ DEFAULT_WARNING_MESSAGE = "Do not treat"
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 TMP_UPLOAD_PATH = os.path.join("uploads", "tmp")
-UPLOAD_ROOT = os.path.join(MEDIA_ROOT, "uploads")
-TMP_UPLOAD_ROOT = os.path.join(UPLOAD_ROOT, "tmp")
-for d in (MEDIA_ROOT, UPLOAD_ROOT, TMP_UPLOAD_ROOT):
-    if not os.path.isdir(d):
-        os.mkdir(d)
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -277,16 +270,6 @@ CACHE_ACTIVE_UTCS_FOR_UNIT_ = 'active_utcs_for_unit_{}'
 MAX_CACHE_TIMEOUT = 24 * 60 * 60  # 24hours
 
 CACHE_LOCATION = os.path.join(PROJECT_ROOT, "cache", "cache_data")
-if not os.path.isdir(CACHE_LOCATION):
-    os.mkdir(CACHE_LOCATION)
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': CACHE_LOCATION,
-        'TIMEOUT': MAX_CACHE_TIMEOUT,
-    }
-}
 
 # -----------------------------------------------------------------------------
 # Session Settings
@@ -531,6 +514,11 @@ USE_SERVICE_LOG = True
 USE_PARTS = True
 USE_ISSUES = False  # internal development issue tracker
 
+DELETE_REASONS = (
+    ('Duplicate', 'Duplicate'),
+    ('Invalid', 'Invalid')
+)
+
 DEFAULT_AVAILABLE_TIMES = {
     'hours_sunday': datetime.timedelta(hours=0, minutes=0),
     'hours_monday': datetime.timedelta(hours=8, minutes=0),
@@ -550,7 +538,6 @@ if os.path.exists('/root/.is_inside_docker'):
 # local_settings contains anything that should be overridden
 # based on site specific requirements (e.g. deployment, development etc)
 
-
 try:
     from .local_settings import *  # NOQA
 except ImportError:
@@ -561,10 +548,35 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # Parts must be used with service log
 USE_PARTS = USE_PARTS or USE_SERVICE_LOG
 
-DELETE_REASONS = (
-    ('Duplicate', 'Duplicate'),
-    ('Invalid', 'Invalid')
-)
+
+# ------------------------------------------------------------------------------
+# Directory availability & dependent paths
+
+# Make any paths available that are not already created
+# Also set file paths that are dependent on other settings which may be overridden
+# in local_settings.py
+
+UPLOAD_ROOT = os.path.join(MEDIA_ROOT, "uploads")
+TMP_UPLOAD_ROOT = os.path.join(UPLOAD_ROOT, "tmp")
+
+if not os.path.isdir(LOG_ROOT):
+    os.mkdir(LOG_ROOT)
+
+for d in (MEDIA_ROOT, UPLOAD_ROOT, TMP_UPLOAD_ROOT):
+    if not os.path.isdir(d):
+        os.mkdir(d)
+
+if not os.path.isdir(CACHE_LOCATION):
+    os.mkdir(CACHE_LOCATION)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': CACHE_LOCATION,
+        'TIMEOUT': MAX_CACHE_TIMEOUT,
+    }
+}
+
 
 if FORCE_SCRIPT_NAME:
     # Fix URL for Admin Views if FORCE_SCRIPT_NAME_SET in local_settings
