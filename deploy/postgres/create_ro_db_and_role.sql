809ce33689627f5625d419d3d5c1265fcad0f4f0
randlet <randle.taylor@gmail.com>
2019-02-06 11:29:44 -0500

    update sqlexplorer perms

--- deploy/postgres/create_ro_db_and_role.sql
+++ deploy/postgres/create_ro_db_and_role.sql
@@ -1,9 +1,12 @@
 CREATE USER qatrack_reports WITH PASSWORD 'qatrackpass';
 GRANT CONNECT ON DATABASE qatrackplus TO qatrack_reports;
+\c qatrackplus
 GRANT USAGE ON SCHEMA public TO qatrack_reports;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO qatrack_reports;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO qatrack_reports;
+REVOKE SELECT ON "django_session" FROM qatrack_reports;
 REVOKE SELECT ON "auth_user" FROM qatrack_reports;
+REVOKE SELECT ON "authtoken" FROM qatrack_reports;
 GRANT SELECT (id, last_login, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined) ON "auth_user" TO qatrack_reports;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO qatrack_reports;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO qatrack_reports;
