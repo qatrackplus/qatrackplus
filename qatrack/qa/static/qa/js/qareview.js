6fd573fcfa296d5ce3ec3d4b2e695d77b1d46c87
randlet <randle.taylor@gmail.com>
2019-11-19 15:50:48 -0500

    Add status select to bottom of review table
    Resolves issue #466

--- qatrack/qa/static/qa/js/qareview.js
+++ qatrack/qa/static/qa/js/qareview.js
@@ -1,42 +1,42 @@
 require(['jquery', 'sl_utils', 'comments'], function($) {
-	$(document).ready(function () {
+    $(document).ready(function () {
 
-		//(de)select checkboxes for child tests when user clicks on header checkbox
-		$("input.test-selected-toggle").on("change", function (e) {
-			$(this).closest("table").find("input.test-selected").prop("checked", $(this).is(":checked"));
-		});
+        //(de)select checkboxes for child tests when user clicks on header checkbox
+        $("input.test-selected-toggle").on("change", function (e) {
+            $(this).closest("table").find("input.test-selected").prop("checked", $(this).is(":checked"));
+        });
 
-		$("#test-list-info-toggle").click(function () {
-			$("#test-list-info").toggle(600);
-		});
-		$("#bulk-status").on('change', function () {
-			var val = $("#bulk-status").val();
-			if (val !== "") {
-				$("input.test-selected:checked").parents("tr").find("select").val(val);
-			}
-			return false;
-		});
+        $("#test-list-info-toggle").click(function () {
+            $("#test-list-info").toggle(600);
+        });
+        $(".bulk-status").on('change', function () {
+            var val = $(".bulk-status").val();
+            if (val !== "") {
+                $("input.test-selected:checked").parents("tr").find("select").val(val);
+            }
+            return false;
+        });
 
-		$('.qa-showcmt > a.revealcomment').click(function () {
-			var this_row = $(this).parent().parent();
-			var comment_row = this_row.next('.qa-comment');
-			comment_row.toggle('fast');
-			comment_row.find('.comment-bar').slideToggle('fast');
-			comment_row.find('.comment-bar').toggleClass('in');
-			this_row.find('.comment-bar').toggleClass('in');
-			return false;
-		});
+        $('.qa-showcmt > a.revealcomment').click(function () {
+            var this_row = $(this).parent().parent();
+            var comment_row = this_row.next('.qa-comment');
+            comment_row.toggle('fast');
+            comment_row.find('.comment-bar').slideToggle('fast');
+            comment_row.find('.comment-bar').toggleClass('in');
+            this_row.find('.comment-bar').toggleClass('in');
+            return false;
+        });
 
-		$('a.revealtext').click(function () {
-			var this_row = $(this).parent().parent().parent();
-			var comment_row = this_row.nextAll(".qa-text-display").eq(0);
-			comment_row.toggle('fast');
-			return false;
-		});
+        $('a.revealtext').click(function () {
+            var this_row = $(this).parent().parent().parent();
+            var comment_row = this_row.nextAll(".qa-text-display").eq(0);
+            comment_row.toggle('fast');
+            return false;
+        });
 
-		var $service_events = $('.service-event-btn');
-		$.each($service_events, function(i, v) {
-			var $service_event = $(this);
+        var $service_events = $('.service-event-btn');
+        $.each($service_events, function(i, v) {
+            var $service_event = $(this);
             var colour = $service_event.attr('data-bgcolour');
             $service_event.css('background-color', colour);
             $service_event.css('border-color', colour);
@@ -61,22 +61,22 @@ require(['jquery', 'sl_utils', 'comments'], function($) {
                 }
             );
         });
-		var $form = $('#qa-review');
-		$('#submit-review-ajax').one('click', function (e) {
-			e.preventDefault();
+        var $form = $('#qa-review');
+        $('#submit-review-ajax').one('click', function (e) {
+            e.preventDefault();
 
-			var data = $form.serialize();
+            var data = $form.serialize();
 
-			$.ajax({
-				type: 'POST',
-				url: QAURLs.TLI_REVIEW + '/' + rtsqa_form + '/' + tli_id + '/',
-				data: data,
-				success: function (res) {
-					QAURLs.returnYourChoice(res.rtsqa_form, res.tli_id);
-				}
-			});
-			return false;
-		});
+            $.ajax({
+                type: 'POST',
+                url: QAURLs.TLI_REVIEW + '/' + rtsqa_form + '/' + tli_id + '/',
+                data: data,
+                success: function (res) {
+                    QAURLs.returnYourChoice(res.rtsqa_form, res.tli_id);
+                }
+            });
+            return false;
+        });
 
-	});
+    });
 });
