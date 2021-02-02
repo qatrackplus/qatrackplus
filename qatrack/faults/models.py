0452b9861130b0658a21b7e18ce208334b416d08
randlet <randle.taylor@gmail.com>
2021-02-02 17:03:31 -0500

    add index on faulttype code

--- qatrack/faults/models.py
+++ qatrack/faults/models.py
@@ -18,6 +18,7 @@ class FaultType(models.Model):
         _l("code"),
         max_length=255,
         help_text=_l('Enter the fault code or number'),
+        db_index=True,
     )
     slug = models.SlugField(
         max_length=255,
