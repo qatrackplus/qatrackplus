533294c3deb3845626f747ed4c9c00b33337be4b
randlet <randle.taylor@gmail.com>
2021-05-05 07:46:14 -0400

    resove warnings about duplicated operation_ids in api

--- qatrack/settings.py
+++ qatrack/settings.py
@@ -271,6 +271,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':
         ('rest_framework.authentication.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication'),
     # Use Django's standard `django.contrib.auth` permissions
+    'DEFAULT_SCHEMA_CLASS': 'qatrack.api.schemas.QATrackAutoSchema',
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissions'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
