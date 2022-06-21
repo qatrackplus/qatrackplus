fad737703929e152a4fb9cfeae487100de3ce72d
Vincent Leduc <leducvin@posteo.net>
2022-06-21 14:28:10 -0400

    update formats/fr

--- qatrack/formats/fr/formats.py
+++ qatrack/formats/fr/formats.py
@@ -1,21 +1,21 @@
 DATETIME_FORMAT = "Y M j H:i"
 DATE_FORMAT = "Y M j"
 TIME_FORMAT = "H:i"
-DATE_INPUT_FORMATS = ["%Y %m %d", "%Y-%m-%d"]
+DATE_INPUT_FORMATS = ["%Y-%m-%d", "%Y %m %d"]
 DATETIME_INPUT_FORMATS = [
-    "%Y %m %d %H:%M",
-    "%Y %m %d %H:%M:%S",
     "%Y-%m-%d %H:%M",
     "%Y-%m-%d %H:%M:%S",
     "%Y-%m-%d %H:%M:%S.%f",
     "%Y-%m-%dT%H:%M:%S.%fZ",
+    "%Y %m %d %H:%M",
+    "%Y %m %d %H:%M:%S",
 ]
 TIME_INPUT_FORMATS = ["%H:%M", "%H:%M:%S", "%H:%M:%S.%f"]
 
 # JavaScript formats
 MOMENT_DATE_DATA_FMT = "YYYY-MM-DD"
-MOMENT_DATE_FMT = "YYYY MM DD"
-MOMENT_DATETIME_FMT = 'YYYY MM DD HH:mm'
-FLATPICKR_DATE_FMT = 'Y M d'
-FLATPICKR_DATETIME_FMT = 'Y M d H:i'
-DATERANGEPICKER_DATE_FMT = 'YYYY MM DD'
+MOMENT_DATE_FMT = "YYYY-MM-DD"
+MOMENT_DATETIME_FMT = 'YYYY-MM-DD HH:mm'
+FLATPICKR_DATE_FMT = 'Y-m-d'
+FLATPICKR_DATETIME_FMT = 'Y-m-d H:i'
+DATERANGEPICKER_DATE_FMT = 'YYYY-MM-DD'
