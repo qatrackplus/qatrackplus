712e62b8d9208bec922124974fea5810ac383ae4
randlet <randle.taylor@gmail.com>
2023-01-22 16:01:57 -0500

    add comment about why this check is made

--- qatrack/qa/static/qa/js/qa.js
+++ qatrack/qa/static/qa/js/qa.js
@@ -833,6 +833,8 @@ require(['jquery', 'lodash', 'moment', 'dropzone', 'autosize', 'cheekycheck', 'i
 
         _.each(tli.test_instances, function(ti){
             if (!skips.hasOwnProperty(ti.test_info.test.slug)) {
+              // if test is not in this set of skips (e.g. after a composite
+              // calc) then we can't set its skip value
               return;
             }
             var skip = skips[ti.test_info.test.slug];
