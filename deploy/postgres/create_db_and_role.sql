a026d9d3f1d7b8e75cbb35ddbe91338d2f3b9a39
najemm <m.najem@outlook.com>
2026-05-28 20:53:05 +0300

    update db , apache and django-q conf files to point to the new qatrack venv directory

--- deploy/postgres/create_db_and_role.sql
+++ deploy/postgres/create_db_and_role.sql
@@ -1,7 +1,7 @@
 CREATE USER qatrack WITH PASSWORD 'qatrackpass';
-CREATE DATABASE qatrackplus31;
-\c qatrackplus31;
-GRANT ALL PRIVILEGES ON DATABASE qatrackplus31 to qatrack;
+CREATE DATABASE qatrackplus32;
+\c qatrackplus32;
+GRANT ALL PRIVILEGES ON DATABASE qatrackplus32 to qatrack;
 GRANT ALL ON SCHEMA public TO qatrack;
 ALTER USER qatrack CREATEDB;
 --GRANT CREATE ON SCHEMA public TO qatrack;
