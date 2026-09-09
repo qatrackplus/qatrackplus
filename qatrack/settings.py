a7617575f5fdcf8736e57aa22a1090551cfd1269
Cody Crewson <6089951+crcrewso@users.noreply.github.com>
2026-09-09 10:14:05 -0600

    Add TODO for local_settings documentation
    Added a TODO comment for documentation improvement.

--- qatrack/settings.py
+++ qatrack/settings.py
@@ -106,6 +106,7 @@ DATETIME_HELP = "Format DD MMM YYYY hh:mm (hh:mm is 24h time e.g. 31 May 2012 14
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en'
 # Duration of the language cookie 
+# TODO: add nice documentation to the local_settings defaults so deployment is clear. 
 LANGUAGE_COOKIE_AGE = 360 * 24 * 60 * 60 # 1 year
 
 # If you set this to False, Django will make some optimizations so as not
