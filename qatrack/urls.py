d03b7e9f2676f140a61dbfe109e34d7fa9a88439
Vincent Leduc <leducvinc@pm.me>
2026-09-18 14:31:18 -0400

    Remove unused import

--- qatrack/urls.py
+++ qatrack/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
 from django.templatetags.static import static as static_url
 from django.urls import include, path
 from django.urls import re_path as url
